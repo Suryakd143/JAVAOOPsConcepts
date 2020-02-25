dce008bb780577ca837c4c780156fe832a7007ca
Surya kanta Dhal <Suryakd143@users.noreply.github.com>
2020-02-25 19:49:54 +0530

    Update AbstractClass.java

--- src/abstarctClassConcepts/AbstractClass.java
+++ src/abstarctClassConcepts/AbstractClass.java
@@ -8,6 +8,7 @@ abstract public class AbstractClass {
       * Abstract Class may or may not contain abstract Methods.
       * If All Method body are defined it is known as "Concrete Class".
       * Abstract Class never instantiated.(Object not created) because an object don't have abstract methods
+      * JVM does not know how much memory it has to allocate for that abstract method because abstract method does not have body implementation
       * and JVM cannot allocate memory of the abstract method.
       */
 
